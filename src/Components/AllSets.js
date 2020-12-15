c88211b8f49810f492449a2bb1cfa77d02738de1
gunnarsikorski <gunnarsikorski@gmail.com>
2020-12-15 13:08:13 -0600

    Adding Material UI, break from local storage

--- src/Components/AllSets.js
+++ src/Components/AllSets.js
@@ -1,5 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import localStorage from 'local-storage';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader'
+import CardMedia from '@material-ui/core/CardMedia';
+// import CardContent from '@material-ui/core/CardContent';
+// import CardActions from '@material-ui/core/CardActions';
+// import Collapse from '@material-ui/core/Collapse';
+// import Avatar from '@material-ui/core/Avatar';
+// import IconButton from '@material-ui/core/IconButton';
+// import Typography from '@material-ui/core/Typography';
+// import { red } from '@material-ui/core/colors';
+// import FavoriteIcon from '@material-ui/icons/Favorite';
+// import ShareIcon from '@material-ui/icons/Share';
+// import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+// import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 const AllSets = () => {
 	const [legos, setLegos] = useState('');
@@ -24,17 +39,28 @@ const AllSets = () => {
     }
 
 	return (
-		<div>
+		<>
 			{legos.map((lego) => (
-				<div>
-					<h2>{lego.name}</h2>
-                    <p>Set Number: {lego.set_number} | Pieces: {lego.piece_count} | Source: {lego.source} | Release Year: {lego.release_year} | Includes: {lego.minifigures}</p>
-                    <img src={lego.image_url} alt='lego'/>
-                    <button onClick={handleClick}>Add to Wishlist</button>
-				</div>
+				<Card>
+                    <CardHeader 
+                        title={lego.name} 
+                        subheader={lego.piece_count}
+                    />
+                    <CardMedia
+                        style={{ height: 0, paddingTop: '56.25%'}}
+                        image={lego.image_url}
+                        title='lego'
+                    />
+					
+				</Card>
 			))}
-		</div>
+		</>
 	);
 };
 
 export default AllSets;
+
+{/* <h2>{lego.name}</h2>
+                    <p>Set Number: {lego.set_number} | Pieces: {lego.piece_count} | Source: {lego.source} | Release Year: {lego.release_year} | Includes: {lego.minifigures}</p>
+                    <img src={lego.image_url} alt='lego'/>
+                    <button onClick={handleClick}>Add to Wishlist</button> */}
