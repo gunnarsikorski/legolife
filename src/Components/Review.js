35c7b6569010ba03876648fa6971f1db5361ab01
gunnarsikorski <gunnarsikorski@gmail.com>
2020-12-16 11:05:45 -0600

    Working on getting CRUD working for reviews

--- src/Components/Review.js
+++ src/Components/Review.js
@@ -1,32 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios'
+import { Redirect } from 'react-router-dom'
 import TextField from '@material-ui/core/TextField'
 import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button'
+import Icon from '@material-ui/core/Icon';
+import SaveIcon from '@material-ui/icons/Save'
 
 const Review = () => {
+    const [redirect, setRedirect] = useState(false)
+    const [review, setReview] = useState('')
+
+    const reviewUrl = 'https://pure-sierra-61007.herokuapp.com/reviews';
+		fetch(reviewUrl)
+			.then((res) => res.json())
+			.then((res) => {
+				let review = res;
+				setReview(review);
+			})
+			.catch((error) => console.log(error));
+
+    const handleCreate = (event) => {
+        event.preventDefault()
+
+        const data = {
+            title: event.target.title.value,
+            body: event.target.title.value,
+        }
+
+        const url = `https://pure-sierra-61007.herokuapp.com/reviews/${review.id}`
+        axios
+            .post(url, data)
+            .then((res) => setRedirect(true))
+    }
+
+    if (redirect) {
+	    return <Redirect to='/sets' />;
+	}
    
     return (
 			<div>
 				<h2>New Review:</h2>
 				<Grid container spacing={6} justify='center'>
-					<Grid item xs={6}>
-						<form noValidate autoComplete='off'>
-							<TextField id='standard-basic' label='Review Title' />
-						</form>
-					</Grid>
-				</Grid>
-				<Grid container spacing={3} justify='center'>
-					<Grid item xs={9}>
-						<form noValidate autoComplete='off'>
+					<form noValidate autoComplete='off' onSubmit={handleCreate}>
+					    <Grid item xs={9}>
+							<TextField id='title' label='Review Title' />
+						
+					    </Grid>
+				
+					    <Grid item xs={12}>
+						
 							<TextField
-								id='outlined-basic'
+								id='body'
 								label='Write review here'
 								variant='outlined'
 								fullWidth='true'
 								multiline='true'
-							/>
-						</form>
-					</Grid>
-				</Grid>
+                                />
+					    </Grid>
+				
+                        <Button
+                            type='submit'
+                            variant='contained'
+                            color='primary'
+                            size='small'
+                            startIcon={<SaveIcon />}>
+                            Save
+                        </Button>
+                    </form>
+                </Grid>
 			</div>
 		);
 };
